40c4f4d76e432c2d3dee9a933af2b7a63068a7e6
HolyDoly <vladimirudakovhd@gmail.com>
2026-07-01 21:45:28 +0300

    Review fixes

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,31 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    @IBOutlet weak var counterLabel: UILabel!
+    // MARK: - IBOutlets
     
-    @IBOutlet weak var incrementButton: UIButton!
-    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet private weak var counterLabel: UILabel!
     
-    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet private weak var incrementButton: UIButton!
+    @IBOutlet private weak var decrementButton: UIButton!
+    
+    @IBOutlet private weak var historyTextView: UITextView!
+    
+    // MARK: - Properties
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return formatter
+    }()
+    
+    private var currentCounter: Int {
+        Int(counterLabel.text ?? "0") ?? 0
+    }
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Непонятно почему, но при проставлении цвета в storyboard он менялся только после клика на кнопку
-        //Поэтому устанавливаю его здесь
-        incrementButton.tintColor = .red
     }
+    
+    // MARK: - IBActions
 
-    @IBAction func incrementCounter() {
-        let newCounter = currentCounter() + 1
+    @IBAction private func incrementButtonTapped() {
+        let newCounter = currentCounter + 1
         counterLabel.text = "\(newCounter)"
         appendHistory(text: "значение изменено на +1")
     }
     
-    @IBAction func decrementCounter() {
-        let newCounter = currentCounter() - 1
+    @IBAction private func decrementButtonTapped() {
+        let newCounter = currentCounter - 1
         if newCounter < 0 {
             appendHistory(text: "попытка уменьшить значение счётчика ниже 0")
         } else {
@@ -40,24 +54,20 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func resetCounter() {
+    @IBAction private func resetCounterButtonTapped() {
         counterLabel.text = "0"
         appendHistory(text: "значение было сброшено")
     }
     
+    // MARK: - Private Methods
+    
     private func appendHistory(text: String) {
-        let date = getDate()
+        let date = currentDateString()
         historyTextView.text += "\n\(date): \(text)"
         historyTextView.scrollRangeToVisible(NSRange(location: historyTextView.text.count, length: 1))
     }
     
-    private func getDate() -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-        return formatter.string(from: Date())
-    }
-    
-    private func currentCounter() -> Int {
-        Int(counterLabel.text ?? "0") ?? 0
+    private func currentDateString() -> String {
+        return dateFormatter.string(from: Date())
     }
 }
