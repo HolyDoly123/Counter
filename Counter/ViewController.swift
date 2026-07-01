56d47ffe3d4e45e3d73e01e055c2a4329bca731f
HolyDoly <vladimirudakovhd@gmail.com>
2026-07-01 03:51:45 +0300

    Added counter label, increment, decrement and reset buttons and counter change history text view

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var decrementButton: UIButton!
+    
+    @IBOutlet weak var historyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Непонятно почему, но при проставлении цвета в storyboard он менялся только после клика на кнопку
+        //Поэтому устанавливаю его здесь
+        incrementButton.tintColor = .red
     }
 
-
+    @IBAction func incrementCounter() {
+        let newCounter = currentCounter() + 1
+        counterLabel.text = "\(newCounter)"
+        appendHistory(text: "значение изменено на +1")
+    }
+    
+    @IBAction func decrementCounter() {
+        let newCounter = currentCounter() - 1
+        if newCounter < 0 {
+            appendHistory(text: "попытка уменьшить значение счётчика ниже 0")
+        } else {
+            counterLabel.text = "\(newCounter)"
+            appendHistory(text: "значение изменено на -1")
+        }
+    }
+    
+    @IBAction func resetCounter() {
+        counterLabel.text = "0"
+        appendHistory(text: "значение было сброшено")
+    }
+    
+    private func appendHistory(text: String) {
+        let date = getDate()
+        historyTextView.text += "\n\(date): \(text)"
+        historyTextView.scrollRangeToVisible(NSRange(location: historyTextView.text.count, length: 1))
+    }
+    
+    private func getDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return formatter.string(from: Date())
+    }
+    
+    private func currentCounter() -> Int {
+        Int(counterLabel.text ?? "0") ?? 0
+    }
 }
-
